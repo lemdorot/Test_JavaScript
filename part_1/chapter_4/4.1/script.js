52270eeb9488c30ffcb082998b7cc1ea652a267c
lemdorot <lemdorot@gmail.com>
2022-06-25 11:01:44 +0300

    Объекты +

--- part_1/chapter_4/4.1/script.js
+++ part_1/chapter_4/4.1/script.js
@@ -43,3 +43,53 @@ function isEmpty(obj) {
 // user.name = "Pete";
 
 // Будет работать, переменную менять нельзя, но содержимое объекта можно
+
+
+// Сумма свойств объекта
+// важность: 5
+// У нас есть объект, в котором хранятся зарплаты нашей команды:
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+// Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. Должно получиться 390.
+
+// Если объект salaries пуст, то результат должен быть 0.
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+let sum = 0;
+
+for (let key in salaries) {
+  sum += salaries[key];
+}
+
+console.log(sum);
+
+// Умножаем все числовые свойства на 2
+// Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] == 'number') {
+      obj[key] *= 2;
+    }
+  }
+}
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+console.log(menu);
+
