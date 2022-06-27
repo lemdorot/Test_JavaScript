d778a7d1b564b4e2421590370ebc7caf0c6376dc
lemdorot <lemdorot@gmail.com>
2022-06-27 15:43:49 +0300

    Методы прототипов, объекты без свойства __proto__

---
+++ part_1/chapter_8/8.4/script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// Добавьте toString в словарь
+
+let dictionary = Object.create(null, {
+  toString: { // определяем свойство toString
+    value() { // значение -- это функция
+      return Object.keys(this).join();
+    }
+  }
+});
+
+// ваш код, который добавляет метод dictionary.toString
+
+// добавляем немного данных
+dictionary.apple = "Apple";
+dictionary.__proto__ = "test"; // здесь __proto__ -- это обычный ключ
+
+// только apple и __proto__ выведены в цикле
+for(let key in dictionary) {
+  alert(key); // "apple", затем "__proto__"
+}
+
+// ваш метод toString в действии
+alert(dictionary); // "apple,__proto__"
+
+
+// Разница между вызовами
+
+function Rabbit(name) {
+  this.name = name;
+}
+Rabbit.prototype.sayHi = function() {
+  alert(this.name);
+};
+
+let rabbit = new Rabbit("Rabbit");
+
+rabbit.sayHi(); // Rabbit
+Rabbit.prototype.sayHi(); // undefined
+Object.getPrototypeOf(rabbit).sayHi(); // undefined
+rabbit.__proto__.sayHi(); // undefined
