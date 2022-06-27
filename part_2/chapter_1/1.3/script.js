935cc2a2b443f6dc3ffd16d5a19a91f31bb01f9c
lemdorot <lemdorot@gmail.com>
2022-06-27 21:12:11 +0300

    Навигация по DOM-элементам

--- part_2/chapter_1/1.3/script.js
+++ part_2/chapter_1/1.3/script.js
@@ -11,3 +11,25 @@
 console.log(document.body.firstElementChild);
 console.log(document.body.children[1]);
 console.log(document.body.children[1].lastElementChild);
+
+// Вопрос о соседях
+
+// Если elem – произвольный узел DOM-элемента…
+
+// Правда, что elem.lastChild.nextSibling всегда равен null?
+// Правда, что elem.children[0].previousSibling всегда равен null ?
+
+// 1.Верно, elem.lastChild всегда последний
+// 2.Неверно, перед elem.children[0] могут быть другие узлы
+
+// Выделите ячейки по диагонали
+
+let table = document.body.children[2];
+
+    for (let i = 0; i < table.rows.length; i++) {
+      let row = table.rows[i];
+      row.cells[i].style.backgroundColor = 'red';
+    }
+
+
+
