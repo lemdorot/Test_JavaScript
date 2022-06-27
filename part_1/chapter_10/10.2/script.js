1ada6f6a4214333353e51c99d9a29cbb752d9a82
lemdorot <lemdorot@gmail.com>
2022-06-27 20:28:16 +0300

    Пользовательские ошибки, расширение Error

---
+++ part_1/chapter_10/10.2/script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// Наследование от SyntaxError
+
+class FormatError extends SyntaxError {
+    constructor(message) {
+      super(message);
+      this.name = "FormatError";
+    }
+  }
+  
+  let err = new FormatError("ошибка форматирования");
+  
+  console.log( err.message ); // ошибка форматирования
+  console.log( err.name ); // FormatError
+  console.log( err.stack ); // stack
+  
+  console.log( err instanceof SyntaxError ); // true
