6538c3436c84bd0f5d3101a7d1c033fbe34da61d
lemdorot <lemdorot@gmail.com>
2022-06-27 20:53:44 +0300

    Навигация по DOM-элементам

---
+++ part_2/chapter_1/1.3/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Дочерние элементы в DOM
+
+// Напишите код, как получить…
+
+// элемент <div>?
+// <ul>?
+// второй <li> (с именем Пит)?
+
+console.log(document.body.firstElementChild);
+console.log(document.body.children[1]);
+console.log(document.body.children[1].lastElementChild);
