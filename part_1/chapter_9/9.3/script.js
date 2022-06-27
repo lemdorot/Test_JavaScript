27f429aa8ad98f98a30d791ca5bf10f9a3e8da7b
lemdorot <lemdorot@gmail.com>
2022-06-27 16:40:07 +0300

    Статические свойства и методы

---
+++ part_1/chapter_9/9.3/script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// Класс расширяет объект?
+
+// class Rabbit {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// let rabbit = new Rabbit("Rab");
+
+// // метод hasOwnProperty от Object.prototype
+// alert( rabbit.hasOwnProperty('name') ); // true
+
+class Rabbit extends Object {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+
+let rabbit = new Rabbit("Кроль");
+
+console.log( rabbit.hasOwnProperty('name') ); // Ошибка
