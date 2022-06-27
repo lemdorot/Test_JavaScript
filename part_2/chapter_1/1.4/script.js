dd5a935f8400abb2f4542c5a4216118576d25a4b
lemdorot <lemdorot@gmail.com>
2022-06-27 21:24:10 +0300

    Поиск: getElement*, querySelector*

---
+++ part_2/chapter_1/1.4/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Вот документ с таблицей и формой.
+
+// Как найти?…
+
+//1. Таблицу с id="age-table".
+//2. Все элементы label внутри этой таблицы (их три).
+//3. Первый td в этой таблице (со словом «Age»).
+//4. Форму form с именем name="search".
+//5. Первый input в этой форме.
+//6. Последний input в этой форме.
+
+//1. let table = document.querySelector('#age-table')
+//2. document.querySelectorAll('#age-table label')
+//3. table.querySelector('td')
+//4. let form = document.querySelector('form[name="search"]')
+//5. form.querySelector('input')
+//6. form.querySelectorAll('input')[inputs.length-1]
+
+
