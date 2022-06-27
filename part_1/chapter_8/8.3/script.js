baf6ac1d50766c2100ba08e509a2a335fd4ca2c7
lemdorot <lemdorot@gmail.com>
2022-06-27 14:26:29 +0300

    Встроенные прототипы

---
+++ part_1/chapter_8/8.3/script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// Добавить функциям метод "f.defer(ms)"
+
+Function.prototype.defer = function(ms) {
+  setTimeout(this, ms);
+};
+
+function f() {
+  console.log("Hello!");
+}
+
+f.defer(1000);
+
+// Добавьте функциям декорирующий метод "defer()"
+
+Function.prototype.defer = function(ms) {
+  let f = this;
+  return function(...args) {
+    setTimeout(() => f.apply(this, args), ms);
+  }
+};
+
+function f(a, b) {
+  console.log( a + b );
+}
+
+f.defer(1000)(1, 2);
